2c7e6522e736cb4b152a0d375c22ad7a56fc8419
mfarhan <intigration@gmail.com>
2022-03-28 20:39:39 +0500

    Add files via upload
    v1

---
+++ precache-manifest.f1aee0280b269684ebce729418e2e0b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f49b13d3afefac1b2eed3bbc621ef04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8276ee0c04f122581826",
+    "url": "/static/css/2.1ed9b90e.chunk.css"
+  },
+  {
+    "revision": "a881371a28eb4670a9c3",
+    "url": "/static/css/main.02729728.chunk.css"
+  },
+  {
+    "revision": "8276ee0c04f122581826",
+    "url": "/static/js/2.abda3013.chunk.js"
+  },
+  {
+    "revision": "2eec0a263ef1f7393322e8c89c5ff82a",
+    "url": "/static/js/2.abda3013.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a881371a28eb4670a9c3",
+    "url": "/static/js/main.0ec2482d.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  }
+]);
